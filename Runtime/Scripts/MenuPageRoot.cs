95e49e57618a8c0789e1c7b55b6f7bfbca631e72
JanSharp <steinmeister36@gmail.com>
2026-01-23 23:48:42 +0100

    Improve page root API a bit regarding show/hide
    Enable scripts that are not show page by permission scripts to hide
      pages by default using a proper API rather than having to hack it

--- Runtime/Scripts/MenuPageRoot.cs
+++ Runtime/Scripts/MenuPageRoot.cs
@@ -17,13 +17,27 @@ namespace JanSharp
         public int PageIndex => pageIndex;
 
         [HideInInspector][SerializeField] private bool hasAnyShowPageByPermissionsInChildren;
+        private bool IsShownByDefault => !hasAnyShowPageByPermissionsInChildren;
         private uint shouldBeShownCounter;
         public bool ShouldBeShown => shouldBeShownCounter != 0u;
 
+        private bool isInitialized;
+        private bool isForcedHidden;
+
         public void Initialize()
         {
-            if (!hasAnyShowPageByPermissionsInChildren)
+            if (IsShownByDefault && !isForcedHidden)
                 shouldBeShownCounter++; // Do not call UpdateWhichPagesAreShown, the manager itself will run that later.
+            isInitialized = true;
+        }
+
+        public void HideByDefault()
+        {
+            if (isForcedHidden)
+                return;
+            isForcedHidden = true;
+            if (isInitialized && IsShownByDefault)
+                DecrementShouldBeShown();
         }
 
         public void IncrementShouldBeShown()
@@ -31,7 +45,8 @@ namespace JanSharp
             shouldBeShownCounter++;
             if (shouldBeShownCounter != 1u) // Was 0.
                 return;
-            menuManager.UpdateWhichPagesAreShown();
+            if (isInitialized)
+                menuManager.UpdateWhichPagesAreShown();
         }
 
         public void DecrementShouldBeShown()
@@ -45,7 +60,8 @@ namespace JanSharp
             shouldBeShownCounter--;
             if (shouldBeShownCounter != 0u)
                 return;
-            menuManager.UpdateWhichPagesAreShown();
+            if (isInitialized)
+                menuManager.UpdateWhichPagesAreShown();
         }
     }
 }
